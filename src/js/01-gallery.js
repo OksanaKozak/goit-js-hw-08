3004e4b5ea5d9ff2fa6ea6cd6d70271d61b4f658
OksanaKozak <oksanakozak2016@gmail.com>
2023-03-30 16:03:04 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList= document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
